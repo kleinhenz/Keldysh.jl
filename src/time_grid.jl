090d7379dc3122dd26180d6fcb747a327ce95d72
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2019-04-18 15:52:45 -0400

    [time_grid] store time step for each branch

--- src/time_grid.jl
+++ src/time_grid.jl
@@ -1,14 +1,17 @@
 struct TimeGrid
   contour::Contour
   points::Vector{BranchPoint}
+  step::Vector{ComplexF64}
+
   function TimeGrid(c::Contour; npts_real = 0, npts_imag = 0)
-    p = BranchPoint[]
+    points = BranchPoint[]
+    step = ComplexF64[]
     for b in c.branches
       npts = (b.domain == imaginary_branch) ? npts_imag : npts_real
       @assert npts > 1 "every branch must have at least two points (b = $b, npts = $npts)"
-      append!(p, get_point.(Ref(b), range(0, 1, length=npts)))
+      append!(points, get_point.(Ref(b), range(0, 1, length=npts)))
+      append!(step, (b.max_val - b.min_val) / (npts - 1))
     end
-    return new(c, p)
+    return new(c, points, step)
   end
 end
-
