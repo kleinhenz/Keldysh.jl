692de0ecfbf258bb7637a316ca934487e6250e43
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2019-04-19 14:22:53 -0400

    [test] test dos2gf on delta function

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 #!/usr/bin/env julia
 
-using Keldysh, Test
+using Keldysh, Test, LinearAlgebra
 
 @testset "branch" begin
   tmax = 2.0
@@ -42,23 +42,32 @@ end
 end
 
 @testset "generate_gf" begin
-  tmax = 1.0
-  β = 1.0
+  let tmax = 1.0, β = 1.0, D=10.0, ν = 10.0
 
-  c = twist!(Contour(full_contour, tmax=tmax, β=β))
-  grid = TimeGrid(c, npts_real=51, npts_imag=51)
+    c = twist!(Contour(full_contour, tmax=tmax, β=β))
+    grid = TimeGrid(c, npts_real=51, npts_imag=51)
+    dos = ω -> (1.0/π) / ((1 + exp(ν * (ω - D))) * (1 + exp(-ν * (ω + D))))
 
-  D = 10.0
-  ν = 10.0
-  dos = ω -> (1.0/π) / ((1 + exp(ν * (ω - D))) * (1 + exp(-ν * (ω + D))))
+    @time hyb1 = make_gf(grid, time_invariant=false) do t1, t2
+      dos2gf(t1, t2, dos=dos, β=β)
+    end
 
-  @time hyb1 = make_gf(grid, time_invariant=false) do t1, t2
-    dos2gf(t1, t2, dos=dos, β=β)
+    @time hyb2 = make_gf(grid, time_invariant=true) do t1, t2
+      dos2gf(t1, t2, dos=dos, β=β)
+    end
+
+    @test hyb1 ≈ hyb2
   end
 
-  @time hyb2 = make_gf(grid, time_invariant=true) do t1, t2
-    dos2gf(t1, t2, dos=dos, β=β)
-  end
+  let tmax = 1.0, β = 1.0, ν = 1/1000, ϵ = 2.0
+    c = twist!(Contour(full_contour, tmax=tmax, β=β))
+    grid = TimeGrid(c, npts_real=51, npts_imag=51)
 
-  @test hyb1 ≈ hyb2
+    dos = ω -> (1.0 / (2 * sqrt(π * ν))) * exp(-((ω - ϵ)^2)/(4ν)) # ~δ(ω - ϵ)
+    hyb1 = dos2gf(grid, dos=dos)
+    hyb2 = gf_1level(grid, ϵ=ϵ)
+
+    # gf_1level is gf for a delta function spectrum
+    @test isapprox(hyb1, hyb2, atol=ν, norm=x -> norm(x, Inf))
+  end
 end
