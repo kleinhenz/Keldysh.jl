597c6a5ee34c45c5c12da4e79a6ada817cba5370
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2019-11-20 18:17:00 -0500

    [interface] update exports

--- src/Keldysh.jl
+++ src/Keldysh.jl
@@ -10,7 +10,7 @@ export BranchEnum, forward_branch, backward_branch, imaginary_branch, BranchPoin
 export ContourEnum, full_contour, keldysh_contour, imaginary_contour, Contour, twist, heaviside, θ, get_branch, nbranches
 
 # TimeGrid functions
-export TimeGrid, TimeGridPoint, branch_bounds, integrate, step
+export TimeGrid, TimeGridPoint, branch_bounds, integrate, step, realtimes, imagtimes
 
 # TimeGF functions
 export TimeGF, TimeGFTranspose, jump
@@ -21,7 +21,7 @@ export dos_integrator, flat_dos, gaussian_dos, bethe_dos, chain_dos, square_dos
 # generate_gf functions
 export gf_1level, dos2gf
 
-import Base.size, Base.getindex, Base.setindex!, Base.IndexStyle, Base.step, Base.length, Base.similar, Base.adjoint, Base.read, Base.write
+import Base.in, Base.size, Base.getindex, Base.setindex!, Base.IndexStyle, Base.step, Base.length, Base.similar, Base.adjoint, Base.read, Base.write
 
 include("util.jl")
 include("branch.jl")
