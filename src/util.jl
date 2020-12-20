54f2842619abae1ad4d2ef88a3d81137dabfc08d
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2020-12-20 15:24:17 -0500

    add dfermi

--- src/util.jl
+++ src/util.jl
@@ -1,5 +1,5 @@
-function fermi(epsilon, beta)
-  x = epsilon * beta
-  x < 0.0 ? (1.0 / (1.0 + exp(x))) : (1.0 - 1.0 / (1.0 + exp(-x)))
-end
+fermi(x) = x <= 0 ? (1 / (1 + exp(x))) : 1 - fermi(-x)
+dfermi(x) = - fermi(x) * fermi(-x)
 
+fermi(ϵ, β) = fermi(ϵ*β)
+dfermi(ϵ, β) = β * dfermi(ϵ*β)
