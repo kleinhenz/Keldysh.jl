dff353b125d62a90382baf23629665f50e1b2648
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2021-05-24 13:37:26 -0700

    [storage] further reduce duplication

--- src/storage.jl
+++ src/storage.jl
@@ -5,11 +5,17 @@
 Abstract supertype for storage of different Keldysh components of nonequilibrium Green's functions taking into account symmetries.
 Represents an array with dimensions (norb,norb,N,M).
 Supports basic arithmetic and indexing.
+`scalar=true` statically expresses norb=1.
 """
 abstract type AbstractStorage{T, scalar} end
 
 Base.eltype(::Type{<:AbstractStorage{T,scalar}}) where {T, scalar} = T
 Base.eltype(X::AbstractStorage) = eltype(typeof(X))
+
+Base.getindex(X::AbstractStorage{T,true}, i, j) where {T} = X[1,1,i,j]
+Base.getindex(X::AbstractStorage{T,false}, i, j) where {T} = X[:,:,i,j]
+Base.setindex!(X::AbstractStorage{T,true}, v, i, j) where {T} = X[1,1,i,j] = v
+Base.setindex!(X::AbstractStorage{T,false}, v, i, j) where {T} = X[:,:,i,j] = v
 
 function Base.size(X::AbstractStorage)
   return (X.norb, X.norb, X.N, X.M)
--- src/storage/herm_toeplitz.jl
+++ src/storage/herm_toeplitz.jl
@@ -35,34 +35,14 @@ end
 
 AntiHermitianToeplitzStorage(N::Integer, norb=1, scalar=false) = TimeInvariantAntiHermitian(ComplexF64, N, norb,  scalar)
 
-function Base.getindex(X::AntiHermitianToeplitzStorage{T,scalar}, i, j) where {T, scalar}
-  @boundscheck @assert (1 <= i <= X.N) && (1 <= j <= X.N)
-  i < j && return copy(-adjoint(X[j,i]))
-  n = i-j+1
-  return scalar ? X.data[1,1,n] : X.data[:,:,n]
-end
-
-function Base.getindex(X::AntiHermitianToeplitzStorage{T,scalar}, k, l, i, j) where {T, scalar}
-  @boundscheck @assert (1 <= i <= X.N) && (1 <= j <= X.N) && (1 <= k <= X.norb) && (1 <= l <= X.norb)
-  i < j && return -conj(X[l,k,j,i])
+function Base.getindex(X::AntiHermitianToeplitzStorage{T,scalar}, k, l, i::Int, j::Int) where {T, scalar}
+  i < j && return copy(-adjoint(X[l,k,j,i]))
   n = i-j+1
   return X.data[k,l,n]
 end
 
-function Base.setindex!(X::AntiHermitianToeplitzStorage{T,scalar}, v, i, j) where {T, scalar}
-  @boundscheck @assert (1 <= i <= X.N) && (1 <= j <= X.N)
-  i < j && return X[j,i] = -adjoint(v)
-  n = i-j+1
-  if scalar
-    return X.data[1,1,n] = v
-  else
-    return X.data[:,:,n] = v
-  end
-end
-
-function Base.setindex!(X::AntiHermitianToeplitzStorage{T,scalar}, v, k, l, i, j) where {T, scalar}
-  @boundscheck @assert (1 <= i <= X.N) && (1 <= j <= X.N) && (1 <= k <= X.norb) && (1 <= l <= X.norb)
-  i < j && return X[l,k,j,i] = -conj(v)
+function Base.setindex!(X::AntiHermitianToeplitzStorage{T,scalar}, v, k, l, i::Int, j::Int) where {T, scalar}
+  i < j && return X[l,k,j,i] = -adjoint(v)
   n = i-j+1
   X.data[k,l,n] = v
 end
